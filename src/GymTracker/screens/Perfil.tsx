5a4c27edc243cd93fe06ec0999244cbabc834520
CauaNicolas <79241219+CauaNicolas@users.noreply.github.com>
2024-05-24 00:32:56 -0300

    criando botões do perfil #6

--- src/GymTracker/screens/Perfil.tsx
+++ src/GymTracker/screens/Perfil.tsx
@@ -1,10 +1,53 @@
-import { View, Text} from "react-native";
+import React from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity} from "react-native";
 
 export default function Perfil(){
     return (
         
-        <View className="w-full h-full bg-zinc-950 items flex items-center justify-center">
+        <><View style={styles.container}>
             <Text className="text-white">Perfil</Text>
         </View>
-    )
+        <View>
+            <TouchableOpacity>
+                <Text>Editar perfil</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity>
+                <Text>Definições gerais</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity>
+                <Text>Idioma</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity>
+                <Text>Nos Avalie</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity>
+                <Text>Sair</Text>
+            </TouchableOpacity>
+        </View>
+        
+        
+        </>
+
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 39,
+        backgroundColor: '#09090B',
+        alignItems:'center'
+    },
+    button: {
+        backgroundColor:'red',
+        padding: 40,
+    },
+});
