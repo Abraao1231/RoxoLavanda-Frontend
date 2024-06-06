df3ca0d11e99d0616dd926b1bf0cb9e72781f1e7
CauaNicolas <79241219+CauaNicolas@users.noreply.github.com>
2024-06-06 15:27:25 -0300

    teste

--- src/GymTracker/screens/DefinicoesGerais.tsx
+++ src/GymTracker/screens/DefinicoesGerais.tsx
@@ -1,10 +1,10 @@
 import { View, Text} from "react-native";
 
-export default function Login(){
+export default function DefinicoesGerais(){
     return (
         
         <View className="w-full h-full bg-zinc-950 items flex items-center justify-center">
-            <Text className="text-white">Login</Text>
+            <Text className="text-white">Definições gerais</Text>
         </View>
     )
 }
