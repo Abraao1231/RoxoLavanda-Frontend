31509eabbccdf62bd6d04894cb11b8ef07792034
CauaNicolas <79241219+CauaNicolas@users.noreply.github.com>
2024-06-14 23:45:57 -0300

    test das bandeiras

--- src/GymTracker/screens/Idioma.tsx
+++ src/GymTracker/screens/Idioma.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image, ScrollView} from "react-native";
+import { View, Text, TouchableOpacity, Image, ScrollView, FlatList} from "react-native";
 import styles from "../screens/style"
 import CountryFlag from "react-native-country-flag";
 import Bandeiras from "../screens/bandeiras";
 
-export default function Login(){
+export default function Idiomas(){
 
 
-
-
+    
     return (
         
-        <View style={styles.container}>
-            <Text style={styles.titulo}>Idiomas</Text>
-            <CountryFlag isoCode="BR" size={25} />
-            <CountryFlag isoCode="BO" size={25} />
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.titulo}>Idiomas</Text>
+                    {
+                        ["AD","AE","AF","AR","BE","BG","BM","BO","BR","BY","BZ","CA","CG","CL","CN","CU","DE","ES","FI","FR","HK","ID","IN","JO","JP","KW","LK","LU","LY","MN","US","VE","YE"].map((item) => (<CountryFlag isoCode={item} size={30}/>))
+                    }
+            </View>
+        </ScrollView>
+    
     )
 }
