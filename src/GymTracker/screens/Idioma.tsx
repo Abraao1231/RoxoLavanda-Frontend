0fea43b16e26eda3136298e18bf48cd16b3a106d
Abraao1231 <abraao1231@gmail.com>
2024-06-19 22:34:51 -0300

    [FIX] ajustando botao de troca de idioma

--- src/GymTracker/screens/Idioma.tsx
+++ src/GymTracker/screens/Idioma.tsx
@@ -1,13 +1,35 @@
-import React, {useState} from "react";
-import { View, Text, TouchableOpacity, ScrollView} from "react-native";
+import React, {useCallback, useEffect, useState} from "react";
+import { View, Text, TouchableOpacity, ScrollView, Alert} from "react-native";
 import styles from "../screens/style"
 import CountryFlag from "react-native-country-flag";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
-
-
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 export default function Idiomas(){
-
+    const [isoCode, setIscoCode] = useState('');
+    async function getLanguage() {
+        try {
+            const lang = await AsyncStorage.getItem('language')
+            if (lang)
+                setIscoCode(lang);    
+            else
+                setIscoCode('br'); 
+        } catch (error) {
+            setIscoCode('br');
+        }
+        
+    }
+    async function handleSaveLang(item : {isoCode: string, nome: string}){
+        try {
+            await AsyncStorage.setItem('language', item.isoCode)
+            setIscoCode(item.isoCode)
+        } catch (error) {
+            Alert.alert("Não foi possivel alterar o idioma")
+        }
+    }
+    useFocusEffect(useCallback(() => {
+        getLanguage();
+    },[]))
     return (    
         <ScrollView>
             <View style={styles.container}>
@@ -55,19 +77,27 @@ export default function Idiomas(){
                             {isoCode: "ve", nome: "Venezuela"},
                             {isoCode: "zw", nome: "Zimbábue"},
                         ]
-                        .map((item) => (<TouchableOpacity style={styles.row}>
-                        <CountryFlag isoCode={item.isoCode} size={25}/>
-                        <Text style={styles.nome}>{item.nome}</Text>
-                        <BouncyCheckbox
-                            size={20}
-                            fillColor="#6D28D9"
-                            unFillColor="#FFFFFF"
-                            iconStyle={{ borderColor: "#6D28D9" }}
-                            innerIconStyle={{ borderWidth: 1 }}
-                            textStyle={{ }}
-                            onPress={(isChecked: boolean) => {console.log(isChecked)}}
-                        style={styles.radioCheck}/>
-                    </TouchableOpacity>))
+                        .map((item) => (
+                        <TouchableOpacity 
+                            key={item.isoCode}
+                            style={styles.row}
+                            onPress={() => handleSaveLang(item)}
+
+                            >
+                                
+                            <CountryFlag isoCode={item.isoCode} size={25}/>
+                            <Text style={styles.nome}>{item.nome}</Text>
+                            <BouncyCheckbox
+                                size={20}
+                                fillColor="#6D28D9"
+                                unFillColor="#FFFFFF"
+                                iconStyle={{ borderColor: "#6D28D9" }}
+                                innerIconStyle={{ borderWidth: 1 }}
+                                textStyle={{ }}
+                                isChecked={item.isoCode == isoCode}
+                                onPress={() => handleSaveLang(item)}
+                                style={styles.radioCheck}/>
+                            </TouchableOpacity>))
                     }
             </View>
         </ScrollView>
