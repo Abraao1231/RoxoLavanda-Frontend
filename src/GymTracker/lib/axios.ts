8212ffe5e3bb01bdea4f8d0223852d8570c053a1
Abraao1231 <abraao1231@gmail.com>
2024-06-30 18:47:59 -0300

    adicionando config

--- src/GymTracker/lib/axios.ts
+++ src/GymTracker/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: 'http://localhost:3333'
-    baseURL: 'https://roxolavanda-backend-1.onrender.com'
+    baseURL: 'http://roxolavanda-backend-1.onrender.com'
     // baseURL: 'http://192.168.1.118:3333'
     // baseURL: 'http://10.0.2.2:3333'
     // baseURL: 'http://192.168.1.113:3333'
